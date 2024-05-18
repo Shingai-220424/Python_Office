bc9377377289e287412fba06bd888398f49ad313
Shingai-220424 <OCA2204240009@edu.oca.ac.jp>
2024-05-18 14:57:37 +0900

    エラー修正

--- test_excel.py
+++ test_excel.py
@@ -20,7 +20,7 @@ ws1 = wb.create_sheet("Mysheet")
 count = 1
 for row in range(1, 11):
     for col in range(1, 11):
-        ws1.cell(row=row, column=col, Value=count)
+        ws1.cell(row=row, column=col, value=count)
         count += 1
 
 
